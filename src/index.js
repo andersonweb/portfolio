b4058158891dababca09c32f3906062907a6be5f
Anderson Fernandes <andersonfer.web@gmail.com>
2021-07-27 12:53:06 -0300

    Tela de informações de cada projeto criada

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import 'jquery/src/jquery';
 
 $(document).ready(() => {
 
-
+	/* Abrir Portfólio */
+	$('#projs .btn-toggle-portfolio').click(() => {
+		$('#projs .portfolio-content').toggleClass('portfolio-content-v');
+	});
 
 });
