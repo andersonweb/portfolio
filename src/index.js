9b83d1de4e570a634c1c24dd1f737a1d5d61f5dd
Anderson Fernandes <andersonfer.web@gmail.com>
2021-07-30 16:54:19 -0300

    Área para contato criada

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ $(document).ready(() => {
 		$('.content-'+proj).addClass('d-block').siblings().removeClass('d-block');
 	});
 
-	/* Rolar suavemente 
+	// Rolar suavemente 
 	$("a").click(function() {
 		var $id = $(this).attr('href');
 	    $('html, body').animate({
 	        scrollTop: $($id).offset().top
 	    }, 2000);
-	});*/
+	});
 
 
 });
